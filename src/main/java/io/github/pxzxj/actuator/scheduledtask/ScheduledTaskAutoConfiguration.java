36339363029cacb63dd5a89b60a037e8d90365e6
pxzxj <pudge.zxj@gmail.com>
2024-09-08 13:46:02 +0800

    fix spring boot 2.5- compatibility problem

--- src/main/java/io/github/pxzxj/actuator/scheduledtask/ScheduledTaskAutoConfiguration.java
+++ src/main/java/io/github/pxzxj/actuator/scheduledtask/ScheduledTaskAutoConfiguration.java
@@ -1,12 +1,16 @@
 package io.github.pxzxj.actuator.scheduledtask;
 
 import ch.qos.logback.classic.Logger;
+import org.springframework.aop.config.AopConfigUtils;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.annotation.AnnotationMatchingPointcut;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
@@ -26,6 +30,7 @@ import java.util.Collection;
 @AutoConfigureAfter({JdbcTemplateAutoConfiguration.class, TransactionAutoConfiguration.class})
 @ConditionalOnClass(Logger.class)
 @EnableConfigurationProperties(ScheduledProperties.class)
+@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
 public class ScheduledTaskAutoConfiguration {
 
     @Bean
@@ -62,6 +67,29 @@ public class ScheduledTaskAutoConfiguration {
         public ScheduledTaskExecutionRepository jdbcScheduledTaskExecutionRepository(ScheduledProperties scheduledProperties, JdbcTemplate jdbcTemplate, TransactionTemplate transactionTemplate) {
             return new JdbcScheduledTaskExecutionRepository(scheduledProperties, jdbcTemplate, transactionTemplate);
         }
+    }
+
+    /**
+     * backport commit <a href="https://github.com/spring-projects/spring-boot/commit/7aabd8bf2d401504636bfe6ce00e57ea8ac7d473">
+     * Ensure that class proxying is forced before AutoProxyCreator is created</a>
+     */
+    @Configuration(proxyBeanMethods = false)
+    @ConditionalOnMissingClass("org.aspectj.weaver.Advice")
+    @ConditionalOnProperty(prefix = "spring.aop", name = "proxy-target-class", havingValue = "true",
+            matchIfMissing = true)
+    static class ClassProxyingConfigurationBackport {
+
+        @Bean
+        static BeanFactoryPostProcessor forceAutoProxyCreatorToUseClassProxyingBackport() {
+            return (beanFactory) -> {
+                if (beanFactory instanceof BeanDefinitionRegistry) {
+                    BeanDefinitionRegistry registry = (BeanDefinitionRegistry) beanFactory;
+                    AopConfigUtils.registerAutoProxyCreatorIfNecessary(registry);
+                    AopConfigUtils.forceAutoProxyCreatorToUseClassProxying(registry);
+                }
+            };
+        }
+
     }
 
 }
